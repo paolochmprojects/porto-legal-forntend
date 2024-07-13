9e9b2a82df9d04419d4dda42fd3d152abf5376c5
paolochmprojects <paolo.dev.projects@gmail.com>
2024-07-12 23:33:28 -0500

    [ADD] tailwindcss

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,27 +3,27 @@ import HomeView from '../views/HomeView.vue';
 import NotFoundView from '../views/NotFoundView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/*',
-      name: 'notFound',
-      component: NotFoundView,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('../views/AboutView.vue'),
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'notFound',
+            component: NotFoundView,
+        },
+    ],
 });
 
 export default router;
